0c26773c957640ef125f1f6c0ddf2019f817f2d1
Andrew Padgett <andrew.padgett.88@gmail.com>
2024-03-26 13:21:02 -0400

    Changed method of converting to Roman numerals, updated README

--- script.js
+++ script.js
@@ -9,18 +9,32 @@ const decimalToRoman = () => {
         return;
     }
     else {
-        const thousands = Math.floor(decimalInt/1000);
-        const hundreds = Math.floor((decimalInt%1000)/100);
-        const tens = Math.floor((decimalInt%100)/10);
-        const ones = decimalInt%10;
-        outputDiv.textContent = `
-        ${decimalInt} in Roman numerals is ${
-            thousandsPlace(thousands) +
-            hundredsPlace(hundreds) +
-            tensPlace(tens) +
-            onesPlace(ones)
-        }.`
-    }
+        const ref = [
+            ["M", 1000],
+            ["CM", 900],
+            ["D", 500],
+            ["CD", 400],
+            ["C", 100],
+            ["XC", 90],
+            ["L", 50],
+            ["XL", 40],
+            ["X", 10],
+            ["IX", 9],
+            ["V", 5],
+            ["IV", 4],
+            ["I", 1]
+        ];
+        const result = [];
+        let num = decimalInt;
+
+        ref.forEach((arr) => {
+            while (num >= arr[1]) {
+                result.push(arr[0]);
+                num -= arr[1];
+            }
+        });
+
+        outputDiv.textContent = result.join("");}
 };
 
 const isValid = (input) => {
@@ -34,59 +48,5 @@ const isValid = (input) => {
     }
     return errMsg;
 };
-
-const onesPlace = (ones) => {
-    switch (ones) {
-        case 0: return "";
-        case 1: return "I";
-        case 2: return "II";
-        case 3: return "III";
-        case 4: return "IV";
-        case 5: return "V";
-        case 6: return "VI";
-        case 7: return "VII";
-        case 8: return "VIII";
-        case 9: return "IX";
-    }
-}
-
-const tensPlace = (tens) => {
-    switch (tens) {
-        case 0: return "";
-        case 1: return "X";
-        case 2: return "XX";
-        case 3: return "XXX";
-        case 4: return "XL";
-        case 5: return "L";
-        case 6: return "LX";
-        case 7: return "LXX";
-        case 8: return "LXXX";
-        case 9: return "XC"
-    }
-}
-
-const hundredsPlace = (hundreds) => {
-    switch (hundreds) {
-        case 0: return "";
-        case 1: return "C";
-        case 2: return "CC";
-        case 3: return "CCC";
-        case 4: return "CD";
-        case 5: return "D";
-        case 6: return "DC";
-        case 7: return "DCC";
-        case 8: return "DCCC";
-        case 9: return "CM";
-    }
-}
-
-const thousandsPlace = (thousands) => {
-    switch (thousands) {
-        case 0: return "";
-        case 1: return "M";
-        case 2: return "MM";
-        case 3: return "MMM";
-    }
-}
 
 convertBtn.addEventListener("click", decimalToRoman);
